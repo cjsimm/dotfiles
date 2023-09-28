a8364fb04795987eff967d86ca4c14c28d52f2d0
gb <glumbongo93@gmail.com>
2023-09-28 22:10:21 +0100

    got lsp working barely. theme still has issues. only c ls working

--- nvim/lua/lazy-config.lua
+++ nvim/lua/lazy-config.lua
@@ -14,3 +14,21 @@ vim.opt.rtp:prepend(lazypath)
 
 --setup function installs each string/table inside the outer function passed to the table 
 require("lazy").setup("plugins")
+
+local lsp_zero = require('lsp-zero')
+
+lsp_zero.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  -- Replace the language servers listed here 
+  -- with the ones you want to install
+  ensure_installed = {'html', 'rust_analyzer'},
+  handlers = {
+    lsp_zero.default_setup,
+  },
+})
