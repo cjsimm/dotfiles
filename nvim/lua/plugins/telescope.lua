ad33de9053a78a7f2994c2ecfe687a20b41b97ab
gb <glumbongo93@gmail.com>
2023-10-01 21:13:32 +0100

    readded telescope default binds

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -19,9 +19,7 @@ return {
         })
 	    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-	    vim.keymap.set('n', '<leader>fs', function()
-            builtin.grep_string({ search = vim.fn.input(" grep > ")});
-        end
-        )
-      end
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    end
 }
