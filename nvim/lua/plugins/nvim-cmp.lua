e9c8931ff91467959a6f9f6c2252333b7a6c767f
gb <glumbongo93@gmail.com>
2023-11-10 11:54:11 +0000

    added luasnip snippet cycling with tab, added cmp function signature auto popup

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -4,6 +4,7 @@ return {
     dependencies = {
         "hrsh7th/cmp-buffer", --source for the completion engine's recommended text
         "hrsh7th/cmp-path", --source for recommended filesystem paths
+        "hrsh7th/cmp-nvim-lsp-signature-help", -- function signature help 
         "L3MON4D3/LuaSnip", --snippet engine
         "saadparwaiz1/cmp_luasnip", --auto completion source for snippets
         "rafamadriz/friendly-snippets", --better snippets 
@@ -13,7 +14,12 @@ return {
         local luasnip = require("luasnip")
         --ensures comparatibilty of friendly snippets with luasnip
         require("luasnip.loaders.from_vscode").lazy_load()
-
+        --LuaSnip setup
+        -- 
+        luasnip.config.set_config({
+            region_check_events = 'InsertEnter',
+            delete_check_events = 'InsertLeave'
+        })
         --setup for nvim cmp
         cmp.setup({
             --configure how completion menu works 
@@ -40,10 +46,27 @@ return {
                 ['<C-e>'] = cmp.mapping.abort(),
                 -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
                 ['<CR>'] = cmp.mapping.confirm({ select = false }),
+                --jump between arguments when snippet selected 
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if luasnip.expand_or_locally_jumpable() then
+                        luasnip.expand_or_jump()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" }),
+                --reverse cycling through arguments 
+                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                    if luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" })
             }),
             --sources for autocompletion. order of sources control priority in completion
             sources = cmp.config.sources({
                 { name = "nvim_lsp" }, --language server 
+                { name = "nvim_lsp_signature_help" }, --function signature prompt 
                 { name = "luasnip" }, --snippets
                 { name = "buffer" }, -- text inside buffer
                 { name = "path" }, -- system paths
