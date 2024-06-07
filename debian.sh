c8ffbfb8f5671b6d350f849393b5908c281436d7
Christopher Simmons <christopher.simmons@gofibre.co.uk>
2024-06-07 15:07:05 +0100

    debian docker install function

--- debian.sh
+++ debian.sh
@@ -63,10 +63,27 @@ install_env_managers() {
     nvm install node
 }
 
+install_docker() {
+    echo "Installing docker..."
+    sudo apt-get install -y ca-certificates curl
+    sudo install -m 0755 -d /etc/apt/keyrings
+    sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+    sudo chmod a+r /etc/apt/keyrings/docker.asc
+    # Add the repository to Apt sources:
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    sudo docker run hello-world
+}
+
 cd "$HOME"
 base_install
 install_nvim
 install_config
 amend_bashrc
 install_env_managers
+install_docker
 exit 0
