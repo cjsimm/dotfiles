498c2d52af02904258c194756beb80494a9b2046
Christopher Simmons <christopher.simmons@gofibre.co.uk>
2024-06-13 12:10:20 +0100

    added htop to base install list

--- debian.sh
+++ debian.sh
@@ -9,7 +9,7 @@ base_install() {
         libssl-dev zlib1g-dev \
         libbz2-dev libreadline-dev libsqlite3-dev curl git \
         libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev \
-        libffi-dev liblzma-dev cmake
+        libffi-dev liblzma-dev cmake htop
     echo "Installations complete."
 }
 
