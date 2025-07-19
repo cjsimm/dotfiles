9dce9d065337374283801175c70f959d2538973c
cjsimm <cjsimm@github.com>
2025-07-19 11:21:44 +0100

    swap default model to gemini (copilot chat limit). still not working

--- .config/nvim/lua/plugins/ai/codecompanion.lua
+++ .config/nvim/lua/plugins/ai/codecompanion.lua
@@ -1,30 +1,30 @@
 return {
     "olimorris/codecompanion.nvim",
     dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
     },
     opts = {
         --Define which model to be used for each functionality
         strategies = {
             chat = {
-                adapter = "copilot",
+                adapter = "gemini",
             },
             inline = {
-                adapter = "copilot",
+                adapter = "gemini",
             },
             cmd = {
-                adapter = "copilot",
+                adapter = "gemini",
             },
         },
         opts = {
             log_level = "DEBUG",
         },
     },
-   config = function()
+    config = function()
         require("codecompanion").setup({})
         vim.keymap.set(
-            { "n", "v" },            
+            { "n", "v" },
             "<leader>a",
             "<cmd>CodeCompanionChat Toggle<cr>",
             { desc = "Toggle a chat buffer" }
@@ -33,7 +33,7 @@ return {
             { "n", "v" },
             "<C-a>",
             "<cmd>CodeCompanionActions<CR>",
-            {desc = "Open the action palette"}
+            { desc = "Open the action palette" }
         )
-   end
+    end
 }
