70f98fb29f5fca679e7da9cd9ee597dac2cf09c0
cjsimm <cjsimm@github.com>
2025-08-23 09:30:39 +0100

    improve output messages to user advising what to do, add -y flag to rustup installer

--- setup.sh
+++ setup.sh
@@ -3,10 +3,16 @@
 # BY default, it should just bring you to a state where youre now prepared to execute anything
 # in the just file
 echo 'Installing the rust toolchain...'
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . "$HOME/.cargo/env"
 echo 'Installing just commandline tool to enable further execution'
 cargo install just
 echo "##################################"
-echo "System ready for further installation. Run `just installation` to proceed with a standard developer installation for your system. Execute `just -l` to see further recipe options"
+echo "System ready for further installation. Run \`just installation\` to proceed with a standard developer installation for your system. Execute \`just -l\` to see further recipe options"
+echo "##################################"
+if [[ "$SHELL" == *"zsh"* ]]; then
+    echo "Please run \`source ~/.zshrc\` before continuing"
+elif [[ "$SHELL" == *"bash"* ]]; then
+    echo "Please run \`source ~/.bashrc\` before continuing"
+fi
 echo "##################################"
