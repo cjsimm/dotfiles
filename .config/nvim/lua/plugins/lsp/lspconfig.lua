71d694b2fa6b020f80efa87d60d1595ef9ae8459
cjsimm <14805972+cjsimm@users.noreply.github.com>
2026-07-05 20:52:45 +0100

    update lspconfig to use new >0.11 api

--- .config/nvim/lua/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -1,36 +1,33 @@
 return {
     "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" }, -- Load LS when opening existing or new file
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = {
-        "hrsh7th/cmp-nvim-lsp",             --completion source
+        "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
-        --required imports
-        local lspconfig = require("lspconfig")
         local cmp_nvim_lsp = require("cmp_nvim_lsp")
-        --set keybinds for language server features. modify opts before each bind to describe what it does
-        local opts = { noremap = true, silent = true }
-        local on_attach = function(client, bufnr)
-            opts.buffer = bufnr
-            --display information
-            opts.desc = "Go to declaration"
-            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-            opts.desc = "Show LSP Definitions"
-            vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
-            opts.desc = "See available code actions"
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-            --modify
-            opts.desc = "Smart rename"
-            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-            --diagnostics
-            -- show float on diagnostic jumps (default [d and ]d)
-            vim.diagnostic.config({ jump = { float = true } })
-            opts.desc = "Show buffer diagnostics"
-            vim.keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
-            opts.desc = "Show line diagnostics"
-            vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
 
-            -- set nvim to format on save using default formatter associated with lsp
+        local capabilities = cmp_nvim_lsp.default_capabilities()
+
+        local function buf_map(bufnr, mode, lhs, rhs, desc)
+            vim.keymap.set(mode, lhs, rhs, {
+                buffer = bufnr,
+                noremap = true,
+                silent = true,
+                desc = desc,
+            })
+        end
+
+        local function on_attach(_, bufnr)
+            buf_map(bufnr, "n", "gD", vim.lsp.buf.declaration, "Go to declaration")
+            buf_map(bufnr, "n", "gd", "<cmd>Telescope lsp_definitions<CR>", "Show LSP definitions")
+            buf_map(bufnr, { "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, "Code actions")
+            buf_map(bufnr, "n", "<leader>rn", vim.lsp.buf.rename, "Rename symbol")
+            buf_map(bufnr, "n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", "Buffer diagnostics")
+            buf_map(bufnr, "n", "<leader>d", vim.diagnostic.open_float, "Line diagnostics")
+
+            vim.diagnostic.config({ jump = { float = true } })
+
             vim.api.nvim_create_autocmd("BufWritePre", {
                 buffer = bufnr,
                 callback = function()
@@ -39,31 +36,35 @@ return {
                 desc = "Format on save",
             })
         end
-        --default capabilities from the completion engine that we will assign to most language servers
-        local capabilities = cmp_nvim_lsp.default_capabilities()
-        --diagnostic symbols to appear in the gutter
-        local signs = { Error = "E", Warn = "W", Hint = "H", Info = "I" }
-        for type, icon in pairs(signs) do
-            local hl = "DiagnosticSign" .. type
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+
+        local function enable(name, config)
+            vim.lsp.config(name, vim.tbl_deep_extend("force", {
+                capabilities = capabilities,
+                on_attach = on_attach,
+            }, config or {}))
+            vim.lsp.enable(name)
         end
-        --individual language server configurations
-        lspconfig["html"].setup({
-            capabilities = capabilities, --pass the completion engine capabilities in
-            on_attach = on_attach,       -- pass our on-attach keybinds in,
+
+        local function ansible_root(bufnr, on_dir)
+            local fname = vim.api.nvim_buf_get_name(bufnr)
+            local found = vim.fs.find({ "roles", "playbooks", ".git" }, {
+                path = fname,
+                upward = true,
+            })
+
+            if #found > 0 then
+                on_dir(vim.fs.dirname(found[1]))
+            end
+        end
+
+        enable("html", {
             filetypes = { "html", "templ", "vue" },
         })
-        lspconfig["cssls"].setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-        })
-        lspconfig["tailwindcss"].setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-        })
-        lspconfig["emmet_ls"].setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
+
+        enable("cssls")
+        enable("tailwindcss")
+
+        enable("emmet_ls", {
             filetypes = {
                 "html",
                 "typescriptreact",
@@ -74,78 +75,58 @@ return {
                 "less",
             },
         })
-        lspconfig["ts_ls"].setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
+
+        enable("ts_ls", {
             init_options = {
                 plugins = {
                     {
                         name = "@vue/typescript-plugin",
-                        location = vim.fn.expand("$MASON/packages/vue-language-server") ..
-                            "/node_modules/@vue/language-server",
+                        location = vim.fn.expand("$MASON/packages/vue-language-server")
+                            .. "/node_modules/@vue/language-server",
                         languages = { "vue" },
                     },
                 },
             },
             filetypes = {
-                "typescript", "javascript", "javascriptreact", "typescriptreact", "vue"
-            }
+                "typescript",
+                "javascript",
+                "javascriptreact",
+                "typescriptreact",
+                "vue",
+            },
         })
-        lspconfig["eslint"].setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-        })
-        lspconfig["pyright"].setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-        })
-        lspconfig["clangd"].setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-        })
-        lspconfig["rust_analyzer"].setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
+
+        enable("eslint")
+        enable("pyright")
+        enable("clangd")
+
+        enable("rust_analyzer", {
             settings = {
                 ["rust-analyzer"] = {
                     checkOnSave = {
                         enable = true,
-                        command = "clippy"
-                    }
-                }
-            }
+                        command = "clippy",
+                    },
+                },
+            },
+        })
 
-        })
-        lspconfig["bashls"].setup({
-            capabilities = capabilities,
-            on_attach = on_attach
-        })
-        lspconfig["docker_compose_language_service"].setup({
-            capabilities = capabilities,
-            on_attach = on_attach
-        })
-        lspconfig["dockerls"].setup({
-            capabilities = capabilities,
-            on_attach = on_attach
-        })
-        lspconfig["ansiblels"].setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
+        enable("bashls")
+        enable("docker_compose_language_service")
+        enable("dockerls")
+
+        enable("ansiblels", {
             filetypes = { "yaml", "yml", "ansible" },
-            root_dir = lspconfig.util.root_pattern("roles", "playbooks")
+            root_dir = ansible_root,
         })
-        --specific config for lua to help with nvim config
-        lspconfig["lua_ls"].setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
+
+        enable("lua_ls", {
             settings = {
                 Lua = {
-                    -- make the language server recognize "vim" global
                     diagnostics = {
                         globals = { "vim" },
                     },
                     workspace = {
-                        -- make language server aware of runtime files
                         library = {
                             [vim.fn.expand("$VIMRUNTIME/lua")] = true,
                             [vim.fn.stdpath("config") .. "/lua"] = true,
@@ -153,9 +134,9 @@ return {
                     },
                     format = {
                         enable = true,
-                    }
+                    },
                 },
             },
         })
-    end
+    end,
 }
