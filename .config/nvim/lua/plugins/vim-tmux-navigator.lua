756001eaa8027a16744f650e371bca84293c1dc6
cjsimm <14805972+cjsimm@users.noreply.github.com>
2026-07-26 22:22:05 +0100

    favour herdr over plugin when nvim launched inside herdr

--- .config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ .config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,3 +1,12 @@
 return {
-	'christoomey/vim-tmux-navigator'
+	"christoomey/vim-tmux-navigator",
+	lazy = false,
+	init = function()
+		-- Herdr owns the shared Ctrl-h/j/k/l keys. Keep this plugin available as
+		-- the fallback when Neovim is launched inside an older tmux session.
+		vim.g.tmux_navigator_no_mappings = 1
+	end,
+	config = function()
+		require("config.herdr_navigation").setup()
+	end,
 }
